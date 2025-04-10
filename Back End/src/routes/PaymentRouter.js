a25bd6ecd25ed9368055296ad1868d551bd40e88
dingthanh <trandinhthanh24112003@gmail.com>
2025-04-11 02:51:39 +0700

    Update PaymentRouter.js

--- Back End/src/routes/PaymentRouter.js
+++ Back End/src/routes/PaymentRouter.js
@@ -15,7 +15,7 @@
 // module.exports = router
 const express = require("express");
 const router = express.Router();
-const paymentController = require("../controllers/paymentController");
+const paymentController = require("../controllers/PaymentController");
 
 router.post("/create-payment", paymentController.createPayment);
 module.exports = router;
