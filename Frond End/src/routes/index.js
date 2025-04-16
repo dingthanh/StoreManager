83e3379b56dbe48217626b1db0789f282671028e
dingthanh <trandinhthanh24112003@gmail.com>
2025-04-16 21:28:13 +0700

    Update index.js

--- Frond End/src/routes/index.js
+++ Frond End/src/routes/index.js
@@ -14,171 +14,171 @@ import SignInPage from "../pages/SignInPage/SignInPage";
 import SignUpPage from "../pages/SignUpPage/SignUpPage";
 import TypeProductPage from "../pages/TypeProductPage/TypeProductPage";
 
-export const routes = [
-    {
-        path: '/',
-        page: HomePage,
-        isShowHeader: true,
-    },
-    {
-        path: '/order',
-        page: OrderPage,
-        isShowHeader: true
-    },
-    {
-        path: '/my-order',
-        page: MyOrderPage,
-        isShowHeader: true
-    },
-    {
-        path: '/details-order/:id',
-        page: DetailsOrderPage,
-        isShowHeader: true
-    },
-    {
-        path: '/payment',
-        page: PaymentPage,
-        isShowHeader: true
-    },
-    {
-        path: '/orderSuccess',
-        page: OrderSucess,
-        isShowHeader: true
-    },
-    {
-        path: '/products',
-        page: ProductsPage,
-        isShowHeader: true
-    },
-    {
-        path: '/product/:type',
-        page: TypeProductPage,
-        isShowHeader: true
-    },
-    {
-        path: '/sign-in',
-        page: SignInPage,
-        isShowHeader: false
-    },
-    {
-        path: '/sign-up',
-        page: SignUpPage,
-        isShowHeader: false
-    },
-    {
-        path: '/product-details/:id',
-        page: ProductDetailsPage,
-        isShowHeader: true
-    },
-    {
-        path: '/profile-user',
-        page: ProfilePage,
-        isShowHeader: true
-    },
-    {
-        path: '/system/admin',
-        page: AdminPage,
-        isShowHeader: false,
-        isPrivated: true
-    },
-    {
-        path: '*',
-        page: NotFoundPage
-    },
-    {
-        path: '/payment-result',
-        page: PaymentResult
-    }
-]
-
-
 // export const routes = [
 //     {
-//       path: '/',
-//       page: HomePage,
-//       isShowHeader: true,
-//       protected: false
+//         path: '/',
+//         page: HomePage,
+//         isShowHeader: true,
 //     },
 //     {
-//       path: '/order',
-//       page: OrderPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/order',
+//         page: OrderPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/my-order',
-//       page: MyOrderPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/my-order',
+//         page: MyOrderPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/details-order/:id',
-//       page: DetailsOrderPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/details-order/:id',
+//         page: DetailsOrderPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/payment',
-//       page: PaymentPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/payment',
+//         page: PaymentPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/orderSuccess',
-//       page: OrderSucess,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/orderSuccess',
+//         page: OrderSucess,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/products',
-//       page: ProductsPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/products',
+//         page: ProductsPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/product/:type',
-//       page: TypeProductPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/product/:type',
+//         page: TypeProductPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/sign-in',
-//       page: SignInPage,
-//       isShowHeader: false,
-//       protected: false
+//         path: '/sign-in',
+//         page: SignInPage,
+//         isShowHeader: false
 //     },
 //     {
-//       path: '/sign-up',
-//       page: SignUpPage,
-//       isShowHeader: false,
-//       protected: false
+//         path: '/sign-up',
+//         page: SignUpPage,
+//         isShowHeader: false
 //     },
 //     {
-//       path: '/product-details/:id',
-//       page: ProductDetailsPage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/product-details/:id',
+//         page: ProductDetailsPage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/profile-user',
-//       page: ProfilePage,
-//       isShowHeader: true,
-//       protected: true
+//         path: '/profile-user',
+//         page: ProfilePage,
+//         isShowHeader: true
 //     },
 //     {
-//       path: '/system/admin',
-//       page: AdminPage,
-//       isShowHeader: false,
-//       protected: true
+//         path: '/system/admin',
+//         page: AdminPage,
+//         isShowHeader: false,
+//         isPrivated: true
 //     },
 //     {
-//       path: '*',
-//       page: NotFoundPage,
-//       protected: false
+//         path: '*',
+//         page: NotFoundPage
 //     },
 //     {
-//       path: '/payment-result',
-//       page: PaymentResult,
-//       protected: false
+//         path: '/payment-result',
+//         page: PaymentResult
 //     }
-//   ];
+// ]
+
+
+export const routes = [
+    {
+      path: '/',
+      page: HomePage,
+      isShowHeader: true,
+      protected: false
+    },
+    {
+      path: '/order',
+      page: OrderPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/my-order',
+      page: MyOrderPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/details-order/:id',
+      page: DetailsOrderPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/payment',
+      page: PaymentPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/orderSuccess',
+      page: OrderSucess,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/products',
+      page: ProductsPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/product/:type',
+      page: TypeProductPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/sign-in',
+      page: SignInPage,
+      isShowHeader: false,
+      protected: false
+    },
+    {
+      path: '/sign-up',
+      page: SignUpPage,
+      isShowHeader: false,
+      protected: false
+    },
+    {
+      path: '/product-details/:id',
+      page: ProductDetailsPage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/profile-user',
+      page: ProfilePage,
+      isShowHeader: true,
+      protected: true
+    },
+    {
+      path: '/system/admin',
+      page: AdminPage,
+      isShowHeader: false,
+      protected: true
+    },
+    {
+      path: '*',
+      page: NotFoundPage,
+      protected: false
+    },
+    {
+      path: '/payment-result',
+      page: PaymentResult,
+      protected: false
+    }
+  ];
